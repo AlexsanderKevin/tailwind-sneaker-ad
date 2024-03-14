fcbf40279a53e4ed120cf5844be783671a6b18c9
Kevin Alexsander De Jesus1 <kevin13348@gmail.com>
2024-03-14 10:03:44 -0300

    applied the group-hover effect

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,20 +15,26 @@ module.exports = {
           "to": { transform: "rotate(10deg)", scale: '1.2'},
         },
         sideSlide: {
-          '0%': { transform: "translateX(0)" },
-          '50%': { transform: "translateX(10px)" },
-          '100%': { transform: "translateX(0)" },
+          '0%': { transform: "translatex(0)" },
+          '50%': { transform: "translatex(10px)" },
+          '100%': { transform: "translatex(0)" },
         },
         blink: {
           '0%': { opacity: 1 },
           '50%': { opacity: .5 },
           '100%': { opacity: 1 },
+        },
+        floatRight: {
+          '0%': { transform: "translatex(0px)" },
+          '50%': { transform: "translatex(8px)" },
+          '100%': { transform: "translatex(0px)" },
         }
       },
       animation: {
         tilt: "tilt 0.3s forwards",
         sideSlide: "sideSlide 2s infinite",
-        blink: "blink 1s infinite"
+        blink: "blink 1s infinite",
+        floatRight: "floatRight 2.5s infinite"
       },
     },
   },
